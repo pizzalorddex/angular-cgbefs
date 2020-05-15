c8f84bae3d179e006a2ac63f5f8fbdb329055a36
pizzalorddex <randomlypizzas@gmail.com>
2020-05-15 16:53:55 +0200

    4 files modified, 7 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -8,14 +10,24 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  product;
 
-  constructor(private route: ActivatedRoute,) { }
+  constructor(private route: ActivatedRoute,
+              private cartService: CartService,) { 
+
+              }
+
+  product;
 
   ngOnInit() {
   this.route.paramMap.subscribe(params => {
     this.product = products[+params.get('productId')];
-  });
-}
+    });
+  }
+
+  addToCart(product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!')
+
+  }
 
 }
